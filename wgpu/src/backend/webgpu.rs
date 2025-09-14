bdf430c352130166dd8b36491c7e84829ebdc852
Arthur Brussee <arthur.brussee@gmail.com>
2025-09-14 16:33:34 +0100

    sg-enable

--- wgpu/src/backend/webgpu.rs
+++ wgpu/src/backend/webgpu.rs
@@ -1627,7 +1627,7 @@ impl dispatch::AdapterInterface for WebAdapter {
         )
         .expect("Setting Object properties should never fail.");
 
-        let required_features = FEATURES_MAPPING
+        let mut required_features = FEATURES_MAPPING
             .iter()
             .copied()
             .flat_map(|(flag, value)| {
@@ -1638,6 +1638,9 @@ impl dispatch::AdapterInterface for WebAdapter {
                 }
             })
             .collect::<js_sys::Array>();
+
+        required_features.push(&JsValue::from("subgroups"));
+
         mapped_desc.set_required_features(&required_features);
 
         if let Some(label) = desc.label {
